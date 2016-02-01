ed64e33c1ed4d674329a4643b90cdd029118cd3f
quanlieu <quanlieu@kms-technology.com>
2016-02-01 11:43:27 +0700

    online-note-0, initiate commit.

---
+++ online-note-0/server/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+
+var app = express();
+
+module.exports = app;

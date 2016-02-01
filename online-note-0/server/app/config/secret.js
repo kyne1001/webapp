1e23638097c12cae177470303b60225bf15fe092
quanlieu <quanlieu@kms-technology.com>
2016-02-01 13:50:01 +0700

    Create folder structure

---
+++ online-note-0/server/app/config/secret.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+  return 'gjfyrzlvogIP2YQ8Es1jcaFt0IbBJRspZge4UyGN';
+}
--- online-note-0/server/server.js
+++ online-note-0/server/server.js
@@ -5,4 +5,14 @@ var MongoClient = mongo.MongoClient;
 
 var app = express();
 
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
 module.exports = app;

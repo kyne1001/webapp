05f1cd22dbde3b2f89eaa5a2bf3f5f9eb1bd78ed
quanlieu <quanlieu@kms-technology.com>
2016-02-01 15:56:11 +0700

    online-note-0: Connect database example.

--- online-note-0/server/server.js
+++ online-note-0/server/server.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
+var db = null;
 
 var app = express();
 
 app.use(bodyParser.json());
+
+// Connect to the db
+var Server = mongo.Server,
+  Db = mongo.Db,
+  BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('online-note', server);
+db.open(function(err, db) {
+  if (!err) {
+    console.log('Connected to "online-note" database');
+    db.collection('users', function (err, col) {
+      col.findOne(function (err, item) {
+        console.log(item);
+      });
+    });
+  } else {
+    console.dir(err);
+    process.exit(1);
+  }
+});
+// db.users.insert({
+// username: 'quanlieu',
+// password: '132456',
+// realname: 'Lieu Chi Quan',
+// gender: true,
+// yob: 1988
+// })
 
 // Router
 app.use('/', require('./app/routes'));

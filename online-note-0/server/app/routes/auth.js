d9c962405db56b6f011c4b61ed40990cf864a4aa
quanlieu <quanlieu@kms-technology.com>
2016-02-02 14:22:36 +0700

    online-note-0: login api.

--- online-note-0/server/app/routes/auth.js
+++ online-note-0/server/app/routes/auth.js
@@ -1,9 +1,53 @@
 var jwt = require('jwt-simple');
 
 var auth = {
-  login: function() {
-    console.log('Login function');
+  login: function(req, res) {
+    var username = req.body.username || '';
+    var password = req.body.password || '';
+    var collection = req.db.collection('users');
+
+    if (username === '' || password === '') {
+      res.status(401);
+      res.json({
+        "status": 404,
+        "message": "Invalid credentials"
+      });
+      return;
+    }
+    collection.findOne({username: username, password: password}, function (err, user) {
+      if (!user) {
+        res.status(401);
+        res.json({
+          "status": 401,
+          "message": "Invalid credentials"
+        });
+      } else {
+        res.status(200);
+        res.json(genToken(user));
+      }
+    });
+
   }
+
+}
+
+// private method
+function genToken(user) {
+  var expires = expiresIn(7); // 7 days
+  var token = jwt.encode({
+    exp: expires
+  }, require('../config/secret')());
+
+  return {
+    token: token,
+    expires: expires,
+    user: user
+  };
+}
+ 
+function expiresIn(numDays) {
+  var dateObj = new Date();
+  return dateObj.setDate(dateObj.getDate() + numDays);
 }
 
 module.exports = auth;
--- online-note-0/server/server.js
+++ online-note-0/server/server.js
@@ -5,50 +5,35 @@ var db = null;
 
 var app = express();
 
-app.use(bodyParser.json());
-
 // Connect to the db
-var Server = mongo.Server,
-  Db = mongo.Db,
-  BSON = mongo.BSONPure;
+var MongoClient = mongo.MongoClient;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('online-note', server);
-db.open(function(err, db) {
-  if (!err) {
-    console.log('Connected to "online-note" database');
-    db.collection('users', function (err, col) {
-      col.findOne(function (err, item) {
-        console.log(item);
-      });
-    });
-  } else {
-    console.dir(err);
-    process.exit(1);
-  }
+var x = MongoClient.connect('mongodb://localhost:27017/online-note');
+
+x.then(function (db) {
+    // Router
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(function(req, res, next){
+    req.db = db;
+    next();
+  });
+
+  app.use('/', require('./app/routes'));
+
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // Start server
+  app.set('port', process.env.PORT || 3000);
+
+  var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+  });
+
+  module.exports = app;
+
 });
-// db.users.insert({
-// username: 'quanlieu',
-// password: '132456',
-// realname: 'Lieu Chi Quan',
-// gender: true,
-// yob: 1988
-// })
-
-// Router
-app.use('/', require('./app/routes'));
-
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// Start server
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
-
-module.exports = app;

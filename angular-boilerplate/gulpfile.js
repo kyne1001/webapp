a0691a22ff49c341ca43fc56a3bfc89d88e03130
quanlieu <quanlieu@kms-technology.com>
2016-02-22 10:14:59 +0700

    BoilerPlate, BrowserSync.

--- angular-boilerplate/gulpfile.js
+++ angular-boilerplate/gulpfile.js
@@ -6,32 +6,29 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     sass = require('gulp-sass');
 
-// Webserver and liveload
-var embedlr = require('gulp-embedlr'),
-    refresh = require('gulp-livereload'),
-    lrserver = require('tiny-lr')(),
+// Webserver and browserSync
+var browserSync = require('browser-sync'),
     express = require('express'),
-    livereload = require('connect-livereload'),
-    livereloadport = 35729,
     serverport = 5000;
 
 // Set up an express server (but not starting it yet)
 var server = express();
-// Add live reload
-server.use(livereload({port: livereloadport}));
 // Use our 'dist' folder as rootfolder
 server.use(express.static('./dist'));
-// Because I like HTML5 pushstate .. this redirects everything back to our index.html
+// Redirects everything back to our index.html
 server.all('/*', function(req, res) {
     res.sendFile('index.html', { root: 'dist' });
 });
 
+// Set up browserSync
+browserSync({
+  notify: false,
+  proxy: "http://localhost:" + serverport
+});
+
 // Dev task
 gulp.task('server', function() {
-  // Start webserver
   server.listen(serverport);
-  // Start live reload
-  lrserver.listen(livereloadport);
 })
 
 gulp.task('dev', ['server', 'watch']);
@@ -57,16 +54,15 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('watch', ['lint'], function() {
-  gulp.watch(['app/*.js', 'app/**/*.js'], ['lint', 'browserify']);
-  gulp.watch(['app/index.html', 'app/**/*.html'], ['views']);
-  gulp.watch(['app/assets/styles/*.scss'], ['styles']);
+  gulp.watch(['app/*.js', 'app/**/*.js'], ['lint', 'browserify', browserSync.reload]);
+  gulp.watch(['app/index.html', 'app/**/*.html'], ['views', browserSync.reload]);
+  gulp.watch(['app/assets/styles/*.scss'], ['styles', browserSync.reload]);
 });
 
 // Views task
 gulp.task('views', function() {
   gulp.src('app/index.html')
   .pipe(gulp.dest('dist/'))
-  .pipe(refresh(lrserver));
 });
 
 // Styles task
@@ -74,7 +70,6 @@ gulp.task('styles', function() {
   gulp.src('app/assets/styles/*.scss')
   .pipe(sass({onError: function(e) { console.log(e); } }))
   .pipe(gulp.dest('dist/css/'))
-  .pipe(refresh(lrserver));
 });
 
 gulp.task('default', ['dev']);

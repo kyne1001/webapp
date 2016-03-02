a77db527ab85002e764d49b71e9f711f4df63e30
quanlieu <quanlieu@kms-technology.com>
2016-03-02 11:07:42 +0700

    2-ways-binding

---
+++ 2-ways-binding/data-bind.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var modelWatcher = function() {
+  if (dataBind.dirtyValues.length) {
+    while (dataBind.dirtyValues.length) {
+      var name = dataBind.dirtyValues[0];
+      var els = document.querySelectorAll('[data-bind="' + name + '"]');
+      for (var i = 0; i < els.length; i++) {
+        if (els[i].tagName === 'INPUT') {
+          // TODO
+        } else {
+          els[i].innerText = dataBind.bindObj[name] || "";
+        }
+      }
+
+      dataBind.dirtyValues.shift();
+    }
+
+    dataBind.hasDirty = false;
+    // Cycle everyone 0.1 seconds
+    setTimeout(modelWatcher, 20);
+  } else {
+    setTimeout(modelWatcher, 20);
+  }
+
+};
+
+var viewWatcher = function(els) {
+  for (var i = 0; i < els.length; i++) {
+    var el = els[i];
+    el.addEventListener('keyup', function() {
+      var bindKey = el.getAttribute('data-bind');
+      dataBind.dirtyValues.push(bindKey);
+      dataBind.bindObj[bindKey] = el.value;
+    }, false);
+
+    el.addEventListener('keydown', function() {
+      var bindKey = el.getAttribute('data-bind');
+      dataBind.dirtyValues.push(bindKey);
+      dataBind.bindObj[bindKey] = el.value;
+    }, false);
+
+    // First run to set data
+    dataBind.dirtyValues.push(el.getAttribute('data-bind'));
+  }
+};
+
+// Execute
+(function() {
+  window.dataBind = {
+    dirtyValues: [],
+    bindObj: {}
+  };
+  // DOM tasks
+  var bindingEls = document.querySelectorAll('[data-bind]');
+  viewWatcher(bindingEls);
+  modelWatcher();
+})();
